966ae6a615468bcf60378478e3e66417e7d826c1
Vinícius Dias <diasvinicius0401@gmail.com>
2025-04-08 20:45:55 -0300

    Adição de Função de adicionar tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import "./App.css";
 import Tasks from "./components/Tasks";
 import Header from "./components/Header";
 import Container from "./components/Container";
+import AddTask from "./components/AddTask";
+import { v4 as uuidv4 } from "uuid";
 
 type Task = {
-  id: number;
+  id: string;
   title: string;
   description: string;
   isCompleted: boolean;
@@ -14,31 +16,31 @@ type Task = {
 function App() {
   const [tasks, setTasks] = useState<Task[]>([
     {
-      id: 1,
+      id: "1",
       title: "Estudar React",
       description: "Ler a documentação oficial e criar um projeto simples.",
       isCompleted: false,
     },
     {
-      id: 2,
+      id: "2",
       title: "Configurar Tailwind CSS",
       description: "Integrar o Tailwind em um projeto React com Vite.",
       isCompleted: false,
     },
     {
-      id: 3,
+      id: "3",
       title: "Praticar JavaScript",
       description: "Resolver exercícios de lógica no Codewars.",
       isCompleted: false,
     },
     {
-      id: 4,
+      id: "4",
       title: "Fazer deploy no Vercel",
       description: "Subir o projeto React finalizado no Vercel.",
       isCompleted: false,
     },
     {
-      id: 5,
+      id: "5",
       title: "Criar portfólio",
       description:
         "Montar uma página pessoal com projetos e informações de contato.",
@@ -46,7 +48,7 @@ function App() {
     },
   ]);
 
-  const onChangeCompleted = (taskId: number) => {
+  const onChangeCompleted = (taskId: string) => {
     const newTasks = tasks.map((task) => {
       //selecionar a tarefa clicada
       if (task.id === taskId) {
@@ -58,15 +60,26 @@ function App() {
     setTasks(newTasks);
   };
 
-  const onDeleteTask = (taskId: number) => {
+  const onDeleteTask = (taskId: string) => {
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
+  };
+
+  const onAddTask = (title: string, description: string): void => {
+    const newTask: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
   };
 
   return (
     <>
       <Header />
       <Container>
+        <AddTask onAddTask={onAddTask} />
         <Tasks
           tasks={tasks}
           onChangeCompleted={onChangeCompleted}
